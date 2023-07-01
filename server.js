d2cfc6e79045b32dbff6b41cd7dca50a01e58543
RepoPanda <124810474+RepoPanda@users.noreply.github.com>
2023-07-01 14:30:18 -0700

    updated server and user model was created

--- server.js
+++ server.js
@@ -15,12 +15,12 @@ const PORT = process.env.PORT || 3030;
 const hbs = exphbs.create({ helpers });
 
 const sess = {
-  secret: 'The Secretiest of Super Secret Secrets',
+  secret: process.env.SECRET,
   cookie: {
-    maxAge: 1 * 60 * 60 * 1000, // session will expire after 1 hour
-    httpOnly: true, // The cookie is not available via JavaScript in the browser
-    secure: false, //  The cookie is only sent to the server with an encrypted request over the HTTPS protocol
-    sameSite: 'strict', // The cookie is not sent with requests from external websites
+    // maxAge: 1 * 60 * 60 * 1000, // session will expire after 1 hour
+    // httpOnly: true, // The cookie is not available via JavaScript in the browser
+    // secure: false, //  The cookie is only sent to the server with an encrypted request over the HTTPS protocol
+    // sameSite: 'strict', // The cookie is not sent with requests from external websites
   },
   resave: false,
   saveUninitialized: true,
@@ -37,9 +37,9 @@ app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(_dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(routes);
+// app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () =>
