dbe2d18796c2c8956a462ae4d8901b6a360c9c54
RepoPanda <124810474+RepoPanda@users.noreply.github.com>
2023-07-08 22:12:34 -0700

    home-routes/ route to get all posts for homepage has been defined

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,8 +2,24 @@ const router = require('express').Router();
 const {Comment, Post, User} = require('../models');
 
 // will need to GET all posts for homepage
+router.get('/', async (req,res) => {
+    try {
+        const postData = await Post.findAll({
+            include:[User]
+        });
 
+        const posts = postData.map((post) => post.get({plain: true}));
+
+        res.render('all-post', {posts});
+    } catch(err) {
+        res.status(500).json(err);
+    }    
+});
 
 // will need to GET one post
 
 // will need to GET all posts from logged in user
+
+
+// exports all from home-router
+module.exports = router;
