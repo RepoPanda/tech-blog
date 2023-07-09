c0fe4c30da9f50666ea59db6348b8692778890d2
RepoPanda <124810474+RepoPanda@users.noreply.github.com>
2023-07-08 17:26:59 -0700

    pseudocoded homeroutes.js

---
+++ controllers/home-routes.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+const {Comment, Post, User} = require('../models');
+
+// will need to GET all posts for homepage
+
+
+// will need to GET one post
+
+// will need to GET all posts from logged in user
