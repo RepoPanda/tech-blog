d37bde70b69c28bb6ed93a92de9fed6be51c956d
RepoPanda <124810474+RepoPanda@users.noreply.github.com>
2023-07-16 01:03:52 -0700

    amended date_created on all-posts.handlebars

--- utils/helpers.js
+++ utils/helpers.js
@@ -3,22 +3,6 @@ module.exports = {
     format_date: (date) => {
       // Format date as MM/DD/YYYY
       return date.toLocaleDateString();
-    },
-    format_time: (time) => {
-      const now = new Date(time);
-      let period = 'am';
-      let hour = now.getHours();
-      if (hour > 12) {
-        period = 'pm';
-        hour = hour % 12;
-      }
-      const month = now.getMonth() + 1;
-      const year = now.getFullYear();
-      const day = now.getDate();
-      const minutes = now.getMinutes();
-      return `${month}/${day}/${year} at ${hour}:${minutes} ${period}`; 
-  
-      
     }
   };
   
