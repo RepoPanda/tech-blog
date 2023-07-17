66f518638884bf7a32b2172739f5ae6184eb5971
RepoPanda <124810474+RepoPanda@users.noreply.github.com>
2023-07-17 16:00:45 -0700

    signup.handlebars amendments and sigupHandler has been created within signup.js

---
+++ public/js/signup.js
@@ -0,0 +1,8 @@
+const signupFormHandler = async (event) => {
+    event.preventDefault();
+
+    const usernameSignup = document.querySelector('#username-signup');
+    const passwordSignup = document.querySelector('#password-signup');
+};
+
+
