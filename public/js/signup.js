a8ce803ded7bc2882cf49550e87c1b3dc17ab701
RepoPanda <124810474+RepoPanda@users.noreply.github.com>
2023-07-17 17:17:19 -0700

    SVG logo updated. login form has been updated. Both log in and signup pages are in need of dashboard to testif they are working.

--- public/js/signup.js
+++ public/js/signup.js
@@ -3,6 +3,24 @@ const signupFormHandler = async (event) => {
 
     const usernameSignup = document.querySelector('#username-signup');
     const passwordSignup = document.querySelector('#password-signup');
+
+    const response = await fetch('/api/user', {
+        method: 'POST',
+        body: JSON.stringify({
+            username: usernameSignup.value,
+            password: passwordSignup.value,
+        }),
+        headers: { 'Content-Type': 'application/json' }
+    });
+
+    if (response.ok) {
+        document.location.replace('/dashboard');
+    } else {
+        alert('Sign up has failed.');
+    }
 };
 
 
+document
+.querySelector('#form-signup')
+.addEventListener('submit', signupFormHandler);
