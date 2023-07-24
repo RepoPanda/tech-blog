906ba66533ca861320c3e7e2231e38eba8550313
RepoPanda <124810474+RepoPanda@users.noreply.github.com>
2023-07-23 22:14:53 -0700

    both login and sign up are able to login to dashboard

--- public/js/login.js
+++ public/js/login.js
@@ -2,16 +2,16 @@ const loginFormHandler = async (event) => {
     event.preventDefault();
 
     // Collect values from the login form
-    const username = document.querySelector('#username-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
+    const usernameLogin = document.querySelector('#username-login');
+    const passwordLogin = document.querySelector('#password-login');
 
-    if(username && password) {
+    if(usernameLogin && passwordLogin) {
         // Send a POST request to the API endpoint
-        const response = await fetch('/api/users/login', {
+        const response = await fetch('/api/user/login', {
             method: 'POST',
             body: JSON.stringify({
-                username: username.value.trim(),
-                password: password.value.trim(),
+                username: usernameLogin.value.trim(),
+                password: passwordLogin.value.trim(),
             }),
             headers: { 'Content-Type': 'application/json' },
         });
@@ -19,11 +19,11 @@ const loginFormHandler = async (event) => {
         if(response.ok) {
             document.location.replace('/dashboard');
         } else {
-            alert('Sorry, something went wrong happened while loggin in. Please try again.');
+            alert('Sorry, something went wrong while loggin you in. Please try again.');
         }
     }
 };
 
 document
-    .querySelector('.login-form')
+    .querySelector('#form-login')
     .addEventListener('submit', loginFormHandler);
