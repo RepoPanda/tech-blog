bb82ca2d570f5a875f730feb36e9f1dcff4c0448
RepoPanda <124810474+RepoPanda@users.noreply.github.com>
2023-07-30 20:18:25 -0700

    updated sole-post.HB

--- public/js/comments.js
+++ public/js/comments.js
@@ -1,1 +1,26 @@
-const commentsFormHandler = async (event) => {};
+const commentsFormHandler = async (event) => {
+    event.preventDefault();
+
+    const post_id = document.querySelector('#post-id').value.trim();
+    const body = document.querySelector('#body-comment').value.trim();
+
+    if (body) {
+        await fetch('/api/comments', {
+            method: 'POST',
+            body: JSON.stringify({ 
+                post_id: post_id, 
+                comment: body 
+            }),
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        });
+
+        document.location.reload();
+    }
+
+};
+
+document
+    .querySelector('#comment-form-new')
+    .addEventListener('submit', commentsFormHandler);
